efde139e792d3ef19dd515f2ee021859f1139ba1
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-06-21 00:25:51 +0900

    connect supabase

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import supabase from "$lib/db";
+    import {onMount} from "svelte";
+    let todos = [];
+
+    onMount(async()=>{
+        let { data, error } = await supabase.from('todo').select('*')
+        todos = data;
+    })
+</script>
+<h1>Welcome to SvelteKit-todos</h1>
+{#each todos as todo }
+    <div class="todos">
+        <input type="checkbox" checked={todo.isComplete}>
+        <input type="text" value={todo.client}>
+        <button>Delete</button>
+    </div>
+{:else}
+<p>No Clients found </p>
+{/each}
+<style>
+    .todos{
+        display: flex;
+        margin-bottom: 0.25em;
+    }
+</style>
+
