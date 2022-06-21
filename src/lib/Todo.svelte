c3b0b0d301bae2546f9394592583b6e4b9b0cce8
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-06-21 23:40:31 +0900

    Client Update

---
+++ src/lib/Todo.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let todo,updateTodo;
+    
+</script>
+<div class="todos">
+    <input type="checkbox" checked={todo.isComplete} />
+    <input type="text"
+        value={todo.client} 
+        on:input={(e)=>{
+        todo.client = e.currentTarget.value;
+        updateTodo(todo);
+    }}
+    />
+    <button>Delete</button>
+</div>
+<style>
+    .todos{
+        display: flex;
+        margin-bottom: 0.25em;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
     import supabase from "$lib/db";
+    import Todo from "$lib/Todo.svelte"
     import {onMount} from "svelte";
     let todos = [];
 
@@ -7,21 +8,22 @@
         let { data, error } = await supabase.from('todo').select('*')
         todos = data;
     })
+    const updateTodo = async(todo) =>{
+        try{
+            const { data, error } = await supabase
+                .from('todo')
+                .update({ client: todo.client })
+                .eq('id', todo.id)
+        }catch(err) {console.log(err)}
+    }
 </script>
 <h1>Welcome to SvelteKit-todos</h1>
 {#each todos as todo }
-    <div class="todos">
-        <input type="checkbox" checked={todo.isComplete}>
-        <input type="text" value={todo.client}>
-        <button>Delete</button>
-    </div>
+    <Todo {todo}{updateTodo} />
 {:else}
 <p>No Clients found </p>
 {/each}
 <style>
-    .todos{
-        display: flex;
-        margin-bottom: 0.25em;
-    }
+
 </style>
 
