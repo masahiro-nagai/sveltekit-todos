873a22ae825445172a10ec74dcc9b90d53d71dd4
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-06-25 00:45:24 +0900

    add client

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -1,9 +1,13 @@
 <script>
-    export let todo,updateTodo;
+    export let todo,updateTodo,deleteTodo;
     
 </script>
 <div class="todos">
-    <input type="checkbox" checked={todo.isComplete} />
+    <input type="checkbox" 
+    checked={todo.isComplete} 
+    on:change={(e)=>{
+        todo.isComplete = e.currentTarget.checked;
+        updateTodo(todo)}}/>
     <input type="text"
         value={todo.client} 
         on:input={(e)=>{
@@ -11,7 +15,7 @@
         updateTodo(todo);
     }}
     />
-    <button>Delete</button>
+    <button on:click={()=>deleteTodo(todo)}>Delete</button>
 </div>
 <style>
     .todos{
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,27 +3,67 @@
     import Todo from "$lib/Todo.svelte"
     import {onMount} from "svelte";
     let todos = [];
+    let newClient = "";
 
     onMount(async()=>{
-        let { data, error } = await supabase.from('todo').select('*')
-        todos = data;
+        await getAllTodo();
     })
+
+    const getAllTodo = async() =>{
+        try{let { data, error } = await supabase.from('todo').select('*')
+        todos = data;
+        }catch (err){
+            console.log(err)
+        }
+    }
+    const addNewClient = async() =>{
+        try{
+            const { data, error } = await supabase
+                .from('todo')
+                .insert([{ client: newClient }])
+            await getAllTodo();
+            newClient = "";
+        }catch(err) {console.log(err)}
+    }
     const updateTodo = async(todo) =>{
         try{
             const { data, error } = await supabase
                 .from('todo')
-                .update({ client: todo.client })
+                .update({ client: todo.client ,isComplete: todo.isComplete})
                 .eq('id', todo.id)
+            await getAllTodo();
         }catch(err) {console.log(err)}
+    }
+    const deleteTodo = async(todo) =>{
+        try{
+            const { data, error } = await supabase
+                .from('todo')
+                .delete()
+                .eq('id', todo.id)
+            await getAllTodo();
+        }catch(err) {console.log(err)}
+    }
+    const handleKeyPress = (event) =>{
+        if(event.key === "Enter" && newClient !== ""){
+            addNewClient();
+        }
     }
 </script>
 <h1>Welcome to SvelteKit-todos</h1>
+<div class="add-todo">
+    <input type="text" bind:value={newClient}>
+    <button on:click={() => addNewClient()}>Add Client</button>
+</div>
 {#each todos as todo }
-    <Todo {todo}{updateTodo} />
+    <Todo {todo}{updateTodo} {deleteTodo}/>
 {:else}
 <p>No Clients found </p>
 {/each}
+<svelte:window on:keypress={handleKeyPress} />
 <style>
-
+    .add-todo{
+        display: flex;
+        margin-bottom: 0.5em;
+    }
 </style>
 
